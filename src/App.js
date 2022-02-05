a0806c1e77d6ffd014b74ada546bdc182286cdbb
Roman-Lahoda <lagoda9515@gmail.com>
2022-02-05 07:41:09 +0200

    base

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Test from './components/test/test';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Test />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/test/test.js
@@ -0,0 +1,11 @@
+import s from './test.module.scss';
+
+const Test = () => {
+  return (
+    <div className={s.test}>
+      <p className={s.text}>Test component</p>
+    </div>
+  );
+};
+
+export default Test;

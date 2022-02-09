17f5cf79a84bc865a584d82731141ced032ab05e
Liudmyla Kozachenko <viergee@gmail.com>
2022-02-09 17:35:53 +0200

    add connect to the backend

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,6 +1,8 @@
+import {Link} from'react-router-dom'
 import s from './Balance.module.scss'
 import sprite from './sprite.svg'
 import { ModalBalance } from './ModalBalance/ModalBalance'
+import { getBalance } from '../../services/kapustaAPI'
 
 export function Balance() {
     const onInputChange = (e) => {
@@ -8,10 +10,13 @@ export function Balance() {
         console.log(inputValue)
     }
 
+    const balance = getBalance();
+    console.log(balance);
+
     return (
         <>
         <div className={s.container}>
-                {/* ссылка на страницу отчетов */}
+                {/* <Link className={s.link}></Link> */}
             <a href='' className={s.link}>
             <span className={s.link_text}>Перейти к отчетам</span> 
             <svg className={s.icon}>
@@ -20,10 +25,11 @@ export function Balance() {
             </a>
             <form className={s.form}> 
                 <label htmlFor='balance' className={s.label}>Баланс: </label>
-                <input className={s.input} id='balance' onChange = {onInputChange}></input>
+                <input type = 'text' className={s.input} id='balance' value='123' onChange = {onInputChange}></input>
                 <button type = "submit" className={s.button}>Подтвердить</button>  
             </form>        
         </div>
+        {/* Добавлю условный рендеринг */}
         <ModalBalance></ModalBalance>
         </>
     )

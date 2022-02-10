968d89f0b49c3b994526493792c6b02c8cfaa75e
Roman-Lahoda <lagoda9515@gmail.com>
2022-02-10 11:00:09 +0200

    fix conflict

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Form from './components/Form/Form';
 
 import './App.css';
-import React from 'react';
 
 import Container from 'components/Container/Container';
 import Dashboard from 'components/Dashboard';

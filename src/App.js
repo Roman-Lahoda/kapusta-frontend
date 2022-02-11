81314b72bb596866b988986c0e6a04ada10f90f1
Roman-Lahoda <lagoda9515@gmail.com>
2022-02-11 14:29:46 +0200

    fix path
    delete file jsconfig.json

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import './App.css';
 import Test from './components/test/test';
 import { Balance } from './components/Balance/Balance';
 
-import Container from 'components/Container/Container';
-import Dashboard from 'components/Dashboard';
+// import Container from 'components/Container/Container';
+// import Dashboard from 'components/Dashboard';
 
 function App() {
   return (

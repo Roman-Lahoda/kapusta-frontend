760782bfc206df2388c97bc6aff40853a931d99f
Natalia-doobro <79019782+Natalia-doobro@users.noreply.github.com>
2022-02-16 21:11:57 +0200

    fix-modal

--- src/components/UniversalModal/UniversalModal.js
+++ src/components/UniversalModal/UniversalModal.js
@@ -3,29 +3,27 @@ import sprite from '../../images/logOutSprite.svg';
 
 import s from "./UniversalModal.module.scss";
 
-function UniversalModal({text, onClickYes, onClose, active }) {
+function UniversalModal({ text, onClickYes, onClose, active }) {
   return (
       <div className={active ? `${s.modal} ${s.open}` : `${s.modalNone}`}>
         <button type='button' className={s.closeBtn} onClick={onClose}>
-            <svg className={s.close}>
-                <use href={`${sprite}#icon-cross`} />
-            </svg>
+          <svg className={s.close}>
+            <use href={`${sprite}#icon-cross`} />
+          </svg>
         </button>
 
         <p className={s.text}>{text}</p>
 
         <ul className={s.list}>
-            <li className={s.item}>
-            <button type='button' className={`${s.button} ${s.buttonYes}`} onClick={onClickYes}>Yes</button>
-            </li>
-            <li>
-            <button type='button' className={`${s.button} ${s.buttonNo}`} onClick={onClose}>No</button>
-            </li>
+          <li className={s.item}>
+            <button type='button' className={`${s.button} ${s.buttonYes}`} onClick={onClickYes}>Да</button>
+          </li>
+          <li>
+            <button type='button' className={`${s.button} ${s.buttonNo}`} onClick={onClose}>Нет</button>
+          </li>
         </ul>
 
       </div>
-
-  
   );
 }
 

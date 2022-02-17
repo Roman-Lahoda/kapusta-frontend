766696fb32d41e83ddc94aa763cbb4fc5e232a24
RomanKorotaev <kro1986@ukr.net>
2022-02-17 02:51:28 +0200

    Every third column is colored + horizontal version

--- src/components/Diagram/Diagram.js
+++ src/components/Diagram/Diagram.js
@@ -4,15 +4,10 @@ import { VictoryBar, VictoryChart, VictoryAxis, VictoryLabel } from 'victory';
 import ownVictoryTheme from './ownVictoryTheme.js';
 import s from '../Report/Report.module.scss';
 
-// это пример массива данных о транзакциях, которые могут прийти с бекенда. Здесь он используется для примера и тестирования.
-// в дальнейшем этот массив данных нужно будет, как полагается получить, от бекенда.
 
-import exampleTransactionArray from './exampleTransactionArray.json'; // это пример массива данных о транзакциях, которые могут прийти с бекенда
 
 const Diagram = function ({ arrayOfData }) {
-  console.log('arrayOfData in diagram ', arrayOfData);
-  console.log('Array.isArray(arrayOfData)  :', Array.isArray(arrayOfData));
-  console.log('arrayOfData.length   :', arrayOfData.length);
+
 
   // Эта функция вынимает из массива данных с транзакциями значения свойств sum, description, проверяет
   // уникальность полей с описанием (description). Если в базе встречаются транзакции с однаковым описанием (description)б
@@ -42,7 +37,7 @@ const Diagram = function ({ arrayOfData }) {
 
   // Сортировка сумм от большей к меньшей
   // dataForDiagram.sort((a, b) => b.sum - a.sum);
-  dataForDiagram.sort((a, b) => a.sum - b.sum);
+  dataForDiagram.sort((a, b) => b.sum - a.sum);
 
   return (
     <div>
@@ -64,12 +59,16 @@ const Diagram = function ({ arrayOfData }) {
             data={dataForDiagram}
             barRatio={0.6}
             cornerRadius={{ top: 5 }}
-            // data accessor for x values
             x="description"
-            // data accessor for y values
             y="sum"
             labels={dataForDiagram.map(elem => `${elem.sum} грн.`)}
-            style={{ data: { fill: '#FF751D' } }}
+            // style={{ data: { fill: '#FF751D' } }}
+            style={ 
+              {data:
+                      { fill: ({ index }) => index % 3 === 0  ? "#FF751D" : "#FFDAC0" },
+                    } 
+                  }
+
             events={[
               {
                 target: 'data',
@@ -90,13 +89,14 @@ const Diagram = function ({ arrayOfData }) {
                 },
               },
             ]}
+            
             animate={{
               duration: 2000,
               onLoad: { duration: 1000 },
             }}
 
-            //для мобильной версии - горизонтальное отображение
-            // horizontal
+            //для мобильной версии - горизонтальное отображение 
+            horizontal = { window.innerWidth < 768} 
             // labelComponent={<VictoryLabel dy={-25} dx={0}/>}
           />
         </VictoryChart>
